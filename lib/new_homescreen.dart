9422a57956a9f0b92e08774cd1e6d41b8d53c00f
n1320447 <goveanestor@gmail.com>
2023-03-24 16:39:57 -0500

    3_24_23 push

--- lib/new_homescreen.dart
+++ lib/new_homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class NewHomeScreen extends StatelessWidget {
   const NewHomeScreen({Key? key}) : super(key: key);
@@ -15,8 +16,16 @@ class NewHomeScreen extends StatelessWidget {
             physics: const NeverScrollableScrollPhysics(),
             itemCount: 20,
             itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text('Feed item ${index + 1}'),
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                ),
+                child: ListTile(
+                  title: Text('Feed item ${index + 1}'),
+                ),
               );
             },
           ),
@@ -25,3 +34,30 @@ class NewHomeScreen extends StatelessWidget {
     );
   }
 }
+
+
+// class NewHomeScreen extends StatelessWidget {
+//   const NewHomeScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Column(
+//         children: [
+//           // You can add your scrolling feed here
+//           // For example, you can use a ListView widget:
+//           ListView.builder(
+//             shrinkWrap: true,
+//             physics: const NeverScrollableScrollPhysics(),
+//             itemCount: 20,
+//             itemBuilder: (BuildContext context, int index) {
+//               return ListTile(
+//                 title: Text('Feed item ${index + 1}'),
+//               );
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
---
+++ lib/post.dart
@@ -0,0 +1,11 @@
+class Post {
+  final String title;
+  final String content;
+  final DateTime createdAt;
+
+  Post({
+    required this.title,
+    required this.content,
+    required this.createdAt,
+  });
+}
