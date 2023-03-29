2d028f911e5cf6fd032b63663a7217797cae1228
n1320447 <goveanestor@gmail.com>
2023-03-28 20:52:32 -0500

    removed extra 'homescreen' text at top

--- lib/new_homescreen.dart
+++ lib/new_homescreen.dart
@@ -34,9 +34,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
+      // appBar: AppBar(
+      //   title: Text('Home'),
+      // ),
       body: Column(
         children: [
           Expanded(
