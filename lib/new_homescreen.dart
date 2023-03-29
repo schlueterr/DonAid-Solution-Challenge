568378873e69558544637378d654d96f8edf478c
n1320447 <goveanestor@gmail.com>
2023-03-28 20:13:54 -0500

    added code to be able to pull all users posts on their feed

--- lib/new_homescreen.dart
+++ lib/new_homescreen.dart
@@ -7,12 +7,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Map<String, dynamic>> posts = [];
+  List<ParseObject> posts = [];
 
   TextEditingController _titleController = TextEditingController();
   TextEditingController _contentController = TextEditingController();
 
   String dropdownValue = 'Donate';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
+  Future<void> fetchPosts() async {
+    final response = await ParseObject('post').getAll();
+    if (response.success) {
+      setState(() {
+        posts = response.results!.cast<ParseObject>();
+      });
+    } else {
+      print('Error fetching posts: ${response.error!.message}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,12 +45,12 @@ class _HomePageState extends State<HomePage> {
               itemBuilder: (BuildContext context, int index) {
                 return Card(
                   child: ListTile(
-                    title: Text(posts[index]['title']),
+                    title: Text(posts[index].get<String>('title') ?? ''),
                     subtitle: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(posts[index]['content']),
-                        Text('Type: ${posts[index]['type']}'),
+                        Text(posts[index].get<String>('content') ?? ''),
+                        Text('Type: ${posts[index].get<String>('type') ?? ''}'),
                       ],
                     ),
                   ),
@@ -81,30 +98,23 @@ class _HomePageState extends State<HomePage> {
                     ),
                     SizedBox(width: 8.0),
                     ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          posts.add({
-                            'title': _titleController.text,
-                            'content': _contentController.text,
-                            'type': dropdownValue,
+                      onPressed: () async {
+                        final parseObject = ParseObject('post')
+                          ..set<String>('title', _titleController.text)
+                          ..set<String>('content', _contentController.text)
+                          ..set<String>('type', dropdownValue);
+
+                        final response = await parseObject.save();
+                        if (response.success) {
+                          setState(() {
+                            posts.add(parseObject);
                           });
-                          final parseObject = ParseObject('post')
-                ..set<String>('title', _titleController.text)
-                ..set<String>('content', _contentController.text)
-                ..set<String>('type', dropdownValue);
+                        } else {
+                          print('Error saving post: ${response.error!.message}');
+                        }
 
-                parseObject.save().then((response) {
-                  if (response.success) {
-                    print('saved post');
-                  } else {
-                    print('did not save post');// There was a problem, check response.error.message
-                  }
-                });
-
-
-                          _titleController.clear();
-                          _contentController.clear();
-                        });
+                        _titleController.clear();
+                        _contentController.clear();
                       },
                       child: Text('Add Post'),
                     ),
@@ -118,3 +128,125 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+// import 'package:flutter/material.dart';
+// import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   List<Map<String, dynamic>> posts = [];
+
+//   TextEditingController _titleController = TextEditingController();
+//   TextEditingController _contentController = TextEditingController();
+
+//   String dropdownValue = 'Donate';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Home'),
+//       ),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: ListView.builder(
+//               itemCount: posts.length,
+//               itemBuilder: (BuildContext context, int index) {
+//                 return Card(
+//                   child: ListTile(
+//                     title: Text(posts[index]['title']),
+//                     subtitle: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Text(posts[index]['content']),
+//                         Text('Type: ${posts[index]['type']}'),
+//                       ],
+//                     ),
+//                   ),
+//                 );
+//               },
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.stretch,
+//               children: [
+//                 TextField(
+//                   controller: _titleController,
+//                   decoration: InputDecoration(
+//                     hintText: 'Enter a title',
+//                   ),
+//                 ),
+//                 SizedBox(height: 8.0),
+//                 TextField(
+//                   controller: _contentController,
+//                   decoration: InputDecoration(
+//                     hintText: 'Enter a content',
+//                   ),
+//                 ),
+//                 SizedBox(height: 8.0),
+//                 Row(
+//                   children: [
+//                     Expanded(
+//                       child: DropdownButton<String>(
+//                         value: dropdownValue,
+//                         onChanged: (String? newValue) {
+//                           setState(() {
+//                             dropdownValue = newValue!;
+//                           });
+//                         },
+//                         items: <String>['Donate', 'Receive']
+//                             .map<DropdownMenuItem<String>>((String value) {
+//                           return DropdownMenuItem<String>(
+//                             value: value,
+//                             child: Text(value),
+//                           );
+//                         }).toList(),
+//                       ),
+//                     ),
+//                     SizedBox(width: 8.0),
+//                     ElevatedButton(
+//                       onPressed: () {
+//                         setState(() {
+//                           posts.add({
+//                             'title': _titleController.text,
+//                             'content': _contentController.text,
+//                             'type': dropdownValue,
+//                           });
+//                           final parseObject = ParseObject('post')
+//                 ..set<String>('title', _titleController.text)
+//                 ..set<String>('content', _contentController.text)
+//                 ..set<String>('type', dropdownValue);
+
+//                 parseObject.save().then((response) {
+//                   if (response.success) {
+//                     print('saved post');
+//                   } else {
+//                     print('did not save post');// There was a problem, check response.error.message
+//                   }
+//                 });
+
+
+//                           _titleController.clear();
+//                           _contentController.clear();
+//                         });
+//                       },
+//                       child: Text('Add Post'),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
