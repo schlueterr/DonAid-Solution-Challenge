ba2fbbd84b7ba00406e24d99878adc69a4a23edc
Paloma <palomaresendiz@ymail.com>
2023-03-30 20:18:32 -0500

    account screen update

--- lib/account_screen.dart
+++ lib/account_screen.dart
@@ -1,17 +1,72 @@
 import 'package:flutter/material.dart';
 
+
 class AccountScreen extends StatelessWidget {
-  const AccountScreen({Key? key}) : super(key: key);
-  
+ const AccountScreen({Key? key}) : super(key: key);
+ 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Account')
-      //   ),
-      body: Center(
-        child: Text('This is the account screen'),
-        ),
-    );
-  }
+  return Scaffold(
+    // appBar: AppBar(
+    //   title: const Text('Account')
+    //   ),
+      body: Column(   
+          children:<Widget>[
+            Container(  
+              margin: const EdgeInsets.all(20.0), 
+              alignment: Alignment.centerLeft, 
+              child: const Text("Name",style: TextStyle(fontSize:16),)
+            ),  
+            Row(
+              children: [
+                ElevatedButton(onPressed: () {  },
+                alignment: 
+                child: const Text("Edit"))
+              ],),
+            Container(  
+              margin: const EdgeInsets.all(20.0), 
+              alignment: Alignment.centerLeft,  
+              child: const Text("Email",style: TextStyle(fontSize:16),), 
+               
+            ),  
+            Row(
+              children: [
+                ElevatedButton(onPressed: () {  },
+                child: const Text("Edit"))
+              ],),
+            Container(  
+              margin: const EdgeInsets.all(20.0),  
+              alignment: Alignment.centerLeft, 
+              child: const Text("Phone",style: TextStyle(fontSize:16),),  
+            ),  
+            Row(
+              children: [
+                ElevatedButton(onPressed: () {  },
+                child: const Text("Edit"))
+              ],),
+            Container(  
+              margin: const EdgeInsets.all(20.0),  
+              alignment: Alignment.centerLeft, 
+              child: const Text("Address",style: TextStyle(fontSize:16),),  
+            ),
+            Row(
+              children: [
+                ElevatedButton(onPressed: () {  },
+                child: const Text("Edit"))
+              ],),
+            Container(  
+              margin: const EdgeInsets.all(20.0),
+              alignment: Alignment.centerLeft,   
+              // ignore: prefer_const_constructors
+              child: const Text("Donor/Recipient",style: TextStyle(fontSize:16),),  
+            ),
+            Row(
+              children: [
+                ElevatedButton(onPressed: () {  },
+                child: const Text("Edit"))
+              ],),  
+          ]  
+      ),  
+    );   
+ }
 }
