9c2d03b5106bc7a4dd56aeb62b827d01ab8f825b
b.schlueter <schlueter.braden@gmail.com>
2023-03-30 19:55:32 -0500

    reverting to previous commit

--- lib/account_screen.dart
+++ lib/account_screen.dart
@@ -4,24 +4,23 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 class AccountScreen extends StatelessWidget {
   const AccountScreen({Key? key}) : super(key: key);
 
-void _logout(BuildContext context) async {
-  final currentUser = await ParseUser.currentUser();
-  if (currentUser != null) {
-    try {
-      await currentUser.logout();
-      Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
-    } catch (e) {
-      // Error occurred during logout
-      print(e);
+  void _logout(BuildContext context) async {
+    final currentUser = await ParseUser.currentUser();
+    if (currentUser != null) {
+      try {
+        await currentUser.logout();
+        Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+      } catch (e) {
+        // Error occurred during logout
+        print(e);
+      }
     }
   }
-}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Account'),
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
@@ -30,7 +29,35 @@ void _logout(BuildContext context) async {
         ],
       ),
       body: Center(
-        child: Text('This is the account screen'),
+        child: FutureBuilder<ParseUser>(
+          future: ParseUs  er.currentUser(
+            include: ['email'],
+          ),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+            final currentUser = snapshot.data;
+            if (currentUser != null) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Username: ${currentUser.username}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Email: ${currentUser.email ?? "N/A"}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              );
+            } else {
+              return Text('No user logged in');
+            }
+          },
+        ),
       ),
     );
   }
